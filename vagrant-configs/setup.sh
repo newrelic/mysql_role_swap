4ea3ca5824569e190dcaba82af71b667457e31f1
Jonathan Thurman <jthurman@newrelic.com>
2012-12-10 16:52:44 -0800

    Initial work to get haproxy listening on the vagrant vms

--- vagrant-configs/setup.sh
+++ vagrant-configs/setup.sh
@@ -3,6 +3,7 @@
 ROLE=$1
 ADMIN_USER=root
 MASTER_HOST=192.168.50.31
+MASTER_PORT=3307
 SLAVE_USER=slave
 SLAVE_PASS=slavepw
 FLOATING_IP=192.168.50.30
@@ -15,9 +16,12 @@ FLOATING_DEV=eth1
 echo "Stopping firewall..."
 service iptables stop 2>&1 >> /dev/null
 
-# Install mysql-server if not already installed
-echo "Verifying mysql server is installed..."
-rpm -q --quiet mysql-server || yum install -y mysql-server
+# Install the epel repo if not already available
+rpm -q --quiet epel-release || rpm -iv http://mirror.vcu.edu/pub/gnu+linux/epel/6/i386/epel-release-6-7.noarch.rpm
+
+# Make sure that required packages are installed
+echo "Verifying required packages are installed..."
+yum install -y haproxy socat mysql-server
 
 # Cleanup any old replication settings
 echo "Cleaning up old mysql instance..."
@@ -26,7 +30,7 @@ rm -rf /var/lib/mysql/*
 
 # Start mysqld
 echo "Verifying mysqld is running..."
-service mysqld status 2>&1 >> /dev/null || service mysqld start 2>&1 > /dev/null
+service mysqld status 2>&1 > /dev/null || service mysqld start 2>&1 > /dev/null
 
 # MySQL user setup
 echo "Granting remote root access..."
@@ -43,13 +47,19 @@ if [ ! -L /etc/my.cnf ]; then
   service mysqld restart
 fi
 
+if [ ! -L /etc/haproxy/haproxy.cfg ]; then
+  rm -f /etc/haproxy/haproxy.cfg
+  ln -s /vagrant/vagrant-configs/haproxy.cfg /etc/haproxy/haproxy.cfg
+fi
+
 # Setup one system to be a slave to start with
 if [ "${ROLE}" == "slave" ]; then
   echo "Checking Replication Status..."
   SLAVE_RUNNING=$(/usr/bin/mysql -u $ADMIN_USER -e "SHOW SLAVE STATUS\G" | grep -c -e "Slave_IO_State: Waiting")
   if [ "${SLAVE_RUNNING}" != "1" ]; then
     echo "Initiating Replication..."
-    /usr/bin/mysql -u $ADMIN_USER -e "CHANGE MASTER TO MASTER_HOST='${MASTER_HOST}', MASTER_USER='${SLAVE_USER}', MASTER_PASSWORD='${SLAVE_PASS}', MASTER_LOG_FILE='', MASTER_LOG_POS=4"
+    /usr/bin/mysql -u $ADMIN_USER -e "CHANGE MASTER TO MASTER_HOST='${MASTER_HOST}', MASTER_PORT=${MASTER_PORT}, MASTER_USER='${SLAVE_USER}', MASTER_PASSWORD='${SLAVE_PASS}', MASTER_LOG_FILE='', MASTER_LOG_POS=4"
+    /usr/bin/mysql -u $ADMIN_USER -e "SLAVE START"
   fi
   echo "Forcing slave to be read-only..."
   /usr/bin/mysql -u $ADMIN_USER -e "SET GLOBAL read_only = on"
@@ -62,5 +72,9 @@ if [ "${ROLE}" == "master" ]; then
     echo "Installing Floating VIP ${FLOATING_IP}..."
     /sbin/ip addr add ${FLOATING_IP} dev ${FLOATING_DEV}
   fi
+  echo "Verifying HAProxy is running..."
+  service haproxy status 2>&1 > /dev/null || service haproxy start 2>&1 > /dev/null
+  echo "Disabling HAProxy instance for test-mysql-2..."
+  echo "disable server test-mysql/test-mysql-2" | socat stdio /var/lib/haproxy/stats
 fi
 
